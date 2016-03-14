85232824277a4db0dd0b22afe638aa09cc9f8af4
sebghub <seb.so@hotmail.com>
2016-03-14 16:36:35 +0100

    Member & testBinarySearchTree
    Work in progress

---
+++ src/Member.java
@@ -0,0 +1,71 @@
+import javax.swing.JOptionPane;
+
+import collections.*;
+/**
+ * Varje member har sina egna personuppgifter och kan logga in för att lämna ett Media objekt,
+ * låna ett Media objekt och se vilka Media objekt denne har lånat.
+ * 
+ * @author Sebastian Sologuren & benjamin Sejdic
+ *
+ */
+
+
+public class Member {
+	private String memberID, name, phoneNumber;
+	private ArrayList<String> loanList;//ändra typ till Media sen
+	
+	/**
+	 * Konstruerar en låntagare med personuppgifter samt ett ID.
+	 * @param ID Lånetagarens ID 
+	 * @param name Lånetagarens namn
+	 * @param pNbr Lånetagarens telefonnummer
+	 *
+	 */
+	public Member(String ID,String name,String pNbr){
+		this.memberID=ID;
+		this.name=name;
+		this.phoneNumber=pNbr;
+	}
+	public void setMemberID(String ID){
+		this.memberID=ID;
+	}
+	public String getMemberID(){
+		return this.memberID;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setPhoneNumber(String nbr){
+		this.phoneNumber=nbr;
+	}
+	public String getPhoneNumber(){
+		return this.phoneNumber;
+	}
+	public void addLoan(String mediaObj){//ändra till Media sen
+		if(loanList==null){
+			loanList = new ArrayList<String>();//ändra typ till Media sen
+		}
+		else{
+			loanList.add(mediaObj);
+		}
+	}
+	public void returnLoan(String mediaObj){//ändra typ till Media sen
+		int indexOf =(loanList.indexOf(mediaObj));
+		if(indexOf != -1){
+			//loanList.get(indexOf).//mediaObj//.setBorrowedStatus(false);
+			loanList.remove(indexOf);
+			JOptionPane.showMessageDialog(null, "mediaObj.setBorrowedStatus(false)");
+		}
+		else{
+			JOptionPane.showMessageDialog(null, "Det här objektet finns inte i din lånelista.");
+		}
+	}
+	public ArrayList<String> getLoanList(){//ändra till Media sen
+		return loanList;
+	}
+	
+	
+}
