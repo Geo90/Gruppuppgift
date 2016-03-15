ed4d57705733729313dcb349688d813377383744
Geo90 <George--Albert@hotmail.com>
2016-03-15 10:33:16 +0100

    Reorganization
    Moved Hashtable-classes to package collections

--- src/collections/HashtableOH.java
+++ src/collections/HashtableOH.java
@@ -1,86 +1,87 @@
 package collections;
-
 import java.util.Iterator;
 
+
 /**
- * The class uses a chained hashtable which resolves collisions, more entries
- * can be stored in the bucket.
- * 
- * @author Benjamin Sejdic
+ * Hashtabellen använder öppen hashing
+ * @author Rolf Axelsson
  */
-public class HashtableOH<K, V> implements Map<K, V> {
-	private LinkedList<Entry<K, V>>[] table;
-	private int size;
-
-	/**
-	 * Creates a new instance of HashtableOH
-	 */
-	public HashtableOH(int size) {
-		table = new LinkedList[size];
-		for (int i = 0; i < size; i++) {
-			table[i] = new LinkedList<Entry<K, V>>();
-		}
-	}
-
-	private int hashIndex(K key) {
-		int hashCode = key.hashCode();
-		hashCode = hashCode % table.length;
-		return (hashCode < 0) ? -hashCode : hashCode;
-	}
-
-	public V put(K key, V value) {
-		V res = null;
-		int hashIndex = hashIndex(key);
-		Entry<K, V> entry = new Entry<K, V>(key, value);
-		int index = table[hashIndex].indexOf(entry);
-		if (index == -1) {
-			table[hashIndex].addFirst(entry);
-			size++;
-		} else {
-			res = table[hashIndex].set(index, entry).value;
-		}
-		return res;
-	}
-
-	/**
-	 * Prints every pair of key and value in the hashtable.
-	 */
-	public void list() {
-		Entry<K, V> entry;
-		for (int i = 0; i < table.length; i++) {
-			System.out.print(i + ":");
-			for (int j = 0; j < table[i].size(); j++) {
-				entry = table[i].get(j);
-				System.out.print(" <" + entry.key + "," + entry.value + ">");
-			}
-			System.out.println();
-		}
-	}
+public class HashtableOH<K,V> implements Map<K,V> { 
+    private LinkedList<Entry<K,V>>[] table;
+    private int size;
+    
+    /** Creates a new instance of HashtableOH */
+    public HashtableOH( int size ) {
+        table = (LinkedList<Entry<K,V>>[])new LinkedList[ size ];
+        for( int i = 0; i < size; i++ ) {
+            table[ i ] = new LinkedList<Entry<K,V>>();
+        }
+    }
+    
+    private int hashIndex( K key ) {
+        int hashCode = key.hashCode();
+        hashCode = hashCode % table.length;
+        return ( hashCode < 0 ) ? -hashCode : hashCode;
+    }
+    
+    public V put( K key, V value ) {
+    	V res = null;
+        int hashIndex = hashIndex( key );
+        Entry<K,V> entry = new Entry<K,V>( key, value );
+        int index = table[ hashIndex ].indexOf( entry );
+        if( index == -1 ) {
+            table[ hashIndex ].addFirst( entry );
+            size++;
+        }
+        else {
+        	res = table[ hashIndex ].set( index, entry ).value;
+        }
+        return res;
+    }
+    
+    public void list() {
+        Entry<K,V> entry;
+        for(int i=0; i<table.length; i++) {
+            System.out.print( i + ":");
+            for( int j = 0; j < table[ i ].size(); j++ ) {
+                entry = table[ i ].get( j );
+                System.out.print(" <" + entry.key +"," + entry.value + ">" );
+            }
+            System.out.println();
+        }
+    }
 
 	public V get(K key) {
-		int hashIndex = hashIndex(key);
-		Entry<K, V> entry = new Entry<K, V>(key, null);
-		for (int pos = 0; pos < table[hashIndex].size(); pos++) {
-			if (table[hashIndex].get(pos).equals(entry)) {
-				return table[hashIndex].get(pos).value;
-			}
-		}
-		return null;
+        Entry<K,V> entry;
+		V res = null;
+        for(int i = 0; i < table.length; i++){ // iterera genom arrayen
+            for( int k = 0; k < table[i].size(); k++ ) { // iterara genom den länkade listan
+            	entry = table[i].get(k);
+            	if(entry.key == key){
+            		res = entry.value;
+            	}
+            }
+
+        }
+        return res;
+
 	}
 
 	public V remove(K key) {
-		int counter = 0;
-		Iterator<Entry<K, V>> entry = table[hashIndex(key)].iterator();
-		while (entry.hasNext()) {
-			if (key.equals(entry.next().key)) {
-				size--;
-				return table[hashIndex(key)].get(counter).value;
+		Entry<K, V> entry;
+		V res = null;
+		for (int i = 0; i < table.length; i++) {
+			for (int k = 0; k < table[i].size(); k++) {
+				entry = table[i].get(k);
+				if (entry.key == key) {
+					res = entry.value;
+					table[i].remove(k);
+					size--;
+				}
 			}
 
-			counter++;
 		}
-
-		return null;
+		return res;
 	}
 
 	public int size() {
@@ -92,40 +93,63 @@ public class HashtableOH<K, V> implements Map<K, V> {
 	}
 
 	public boolean containsKey(K key) {
-		return get(key) != null;
+		int hashIndex = hashIndex( key );
+		V value = get(key);
+        Entry<K,V> entry = new Entry<K,V>( key, value );
+        int index = table[ hashIndex ].indexOf( entry );
+        if( index == -1 ) {
+        	return false;
+        }else{
+        	return true;
+        }
 	}
 
 	public void clear() {
 		for (int i = 0; i < table.length; i++) {
-			for (int j = 0; j < table[i].size(); j++) {
-				table[i].get(j).key = null;
-				table[i].get(j).value = null;
-			}
+			table[i].clear();
 		}
 		size = 0;
 	}
 
 	public Iterator<K> keys() {
-		ArrayList<K> keys = new ArrayList<K>();
-		for (int i = 0; i < table.length; i++) {
-			for (int j = 0; j < table[i].size(); j++) {
-				if (table[i].get(j).key != null) {
-					keys.add(table[i].get(j).key);
-				}
-			}
-		}
-		return keys.iterator();
+		return null;
 	}
 
 	public Iterator<V> values() {
-		ArrayList<V> values = new ArrayList<V>();
-		for (int i = 0; i < table.length; i++) {
-			for (int j = 0; j < table[i].size(); j++) {
-				if (table[i].get(j).value != null) {
-					values.add(table[i].get(j).value);
-				}
-			}
-		}
-		return values.iterator();
+        return null;
 	}
+    
+    public static void main(String[] args) {
+        HashtableOH<String,String> table = new HashtableOH<String,String>(4);
+        table.put("hej", "hello");      
+        table.put("röd", "red");        
+        table.put("vit", "white");      
+        table.put("säng", "bed");       
+        table.put("svart", "black");    
+        table.put("gul", "yellow");     
+        table.put("dator", "computer"); 
+        table.put("snö", "snow");       
+        table.put("blå", "blue");       
+        table.put("grön", "green");     
+        table.put("hus", "house");      
+        table.list();
+        
+        System.out.println("Test av get-metoden: ");
+        System.out.println(table.get("hus"));
+        
+        System.out.println("Test av remove-metoden: ");
+        System.out.println(table.remove("hus"));
+        table.list();
+        System.out.println(table.remove("Laserturken"));
+        
+        System.out.println("Test av containsKey-metoden: ");
+        System.out.println(table.containsKey("hus"));
+        System.out.println(table.containsKey("grön"));
+        
+        System.out.println("Test av clear-metoden: ");
+        table.clear();
+        table.list();
+
+
+    }
 }
